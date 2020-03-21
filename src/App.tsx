9de4be26d7ef224fbea9b0c5d7948a1457501ddd
Tulio <tuliocll@gmail.com>
2020-03-21 19:59:27 -0300

    :sparkles: add redux and saga functions (i need refactor this to ts files)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
 
+import { store, persistor } from "./Store";
 import Routes from "./routes";
+import history from "./Services/history";
+
 import "./styles/global.css";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes />
-      </Router>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Router history={history}>
+            <Routes />
+          </Router>
+        </PersistGate>
+      </Provider>
     </div>
   );
 }
